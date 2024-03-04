c49763e9cfb71695b20e92d2fc595ca87293a380
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-03 19:05:35 -0600

    corrected the homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,24 +1,8 @@
 // This comes from Activity 22 in the MVC module:
 
 const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
+const homeRoutes = require('./homeRoutes');
 
-const homeController = {
-    getHomePage: async (req, res) => {
-        try {
-            const posts = await Post.findAll({
-                include: [{
-                    model: User,
-                    attributes: ['username']
-                }],
-                order: [['createdAt', 'DESC']]
-            });
-            res.render('homepage', { posts });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: 'Internal Server Error' });
-        }
-    },
-};
+router.use('/', homeRoutes.getHomePage);
 
-module.exports = homeController;
+module.exports = router;
