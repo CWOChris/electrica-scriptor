fdda26e9746e0dd91237f964e2ba0da87831c112
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-03 19:19:00 -0600

    added the index.js model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 // This comes from Activity 22 in the MVC module:
 
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-module.exports = { User };
+module.exports = { User, Post, Comment };
