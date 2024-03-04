fe850b63dfab8dc41788e9940ead0e36ed1f3126
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-03 19:31:39 -0600

    reverted to correct Comment and Post models

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,29 @@
-const express = require('express');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+const Post = sequelize.define('post', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    post_content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    }
+});
+
+module.exports = Post;
