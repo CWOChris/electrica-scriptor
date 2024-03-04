12307724e19429c11176b10d4207e96d569b8c13
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-03-03 20:46:39 -0600

    chasing errors on userRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,16 @@
 // This comes from Activity 22 in the MVC module:
 
 const router = require('express').Router();
-const homeRoutes = require('./homeRoutes');
 
-router.use('/', homeRoutes.getHomePage);
+const getHomePage = async (req, res) => {
+    try {
+        res.render('homepage', {});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error'});
+    }
+};
+
+router.get('/', getHomePage);
 
 module.exports = router;
--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,8 +1,34 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
 
-router.post('/', userRoutes.signup);
-router.post('/login', userRoutes.login);
-router.get('/logout', userRoutes.logout);
+const createUser = async (req, res) => {
+    try {
+        res.send('User Created Successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        res.send('User Logged in Successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+const logoutUser = async (req, res) => {
+    try {
+        res.send('User logged out successfully!');
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+router.post('/signup', createUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
 module.exports = router;
